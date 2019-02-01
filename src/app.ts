6824c9c45cdaa1f93e51bb7e94344e3a5afa76cf
Henrique Carrasco <henrique.carrasco.1@gmail.com>
2019-02-01 21:32:41 -0200

    working on cardscene

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,18 @@
-import { Application, loader } from 'pixi.js';
+import { Application, loader, Text } from 'pixi.js';
 import { CardScene } from '@app/scenes/cardScene';
 
 class Game {
-  private app: Application;
+  app: Application;
   cardScene: CardScene;
+  fpsText: Text;
   constructor() {
     // instantiate app
     this.app = new Application({
       autoResize: true,
-      resolution: devicePixelRatio,
       width: window.innerWidth,
       height: window.innerHeight,
-      backgroundColor: 0x1099bb 
+      resolution: devicePixelRatio,
+      backgroundColor: 0x1099bb
     });
 
     document.body.appendChild(this.app.view);
@@ -25,15 +26,20 @@ class Game {
 
   setup(): void {
     this.cardScene = new CardScene();
-
+    this.fpsText = new PIXI.Text('oi', { fill: 0xb6f442, fontSize: 14 });
+    this.fpsText.x = this.app.screen.left;
+    this.fpsText.y = this.app.screen.top;
     this.app.stage = this.cardScene;
-    this.app.ticker.add(() => {
-    });
+    this.cardScene.addChild(this.fpsText);
+    this.app.ticker.add(this.update.bind(this));
   }
 
 
   resize() {
     this.app.renderer.resize(window.innerWidth, window.innerHeight);
+  }
+  update() {
+    this.fpsText.text = "FPS: " + this.app.ticker.FPS.toFixed(2);
   }
 }
 
--- src/app/scenes/cardScene.ts
+++ src/app/scenes/cardScene.ts
@@ -1,27 +1,30 @@
-import { Container, Point, Texture, ticker} from "pixi.js";
+import { Container, Point, Texture} from "pixi.js";
 import { Card } from "../components/card";
+import { instance as Game } from "../../app";
 
 const NUMOFCARDS = 144;
 export class CardScene extends Container { 
     cards: Card[]
-    private ticker: ticker.Ticker;
     private cardTexture: Texture;
     constructor() {
         super();
         this.cardTexture = PIXI.loader.resources["assets/img/card.png"].texture;
         this.cards = [];
+        Game.app.ticker.add(this.update.bind(this));
         this.createCards();
-        this.ticker.add(this.update);
     }
 
     createCards() {
         for (var i = 0; i < NUMOFCARDS; i++) {
-            let card = new Card(new Point(0, 0), 0);
+            let card = new Card(new Point(Game.app.screen.width/2 + 0.1 * i - 200, Game.app.screen.height/2), 0);
             card.texture = this.cardTexture;
             this.cards.push(card); 
             this.addChild(card);
         }
         
+    }
+
+    async moveCard() {
     }
 
     update() {
