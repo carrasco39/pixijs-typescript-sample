abf9aa9949a5b9e9a06d135bd5e77be781fe6bc1
Henrique Carrasco <henrique.carrasco.1@gmail.com>
2019-02-03 15:51:55 -0200

    added readme.md

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# PIXI Example
+
+
+http://carrasco.rocks/games/pixi
--- src/app/stages/particleStage.ts
+++ src/app/stages/particleStage.ts
@@ -13,21 +13,30 @@ export class ParticleStage extends BaseStage {
     }
 
     async init() {
-        PIXI.loader
-            .add('fx_settings', 'assets/particles/fire/fire.json')
-            .add('fx_spritesheet', 'assets/particles/fire/revoltfx-spritesheet.json')
-            .load((loaders: PIXI.loaders.Loader,resources: PIXI.loaders.ResourceDictionary) => {
-                console.log(loaders.baseUrl);
-                this.candleFX.initBundle(resources.fx_settings.data);
-                this.emitter = this.candleFX.getParticleEmitter("fire_candle");
-                this.emitter.init(this, false, 1);
-                this.emitter.start();
-                this.emitter.infinite = true;
-                this.emitter.x = Game.app.screen.width / 2;
-                this.emitter.y = Game.app.screen.height / 2;
-            });
+        if (!PIXI.loader.resources.fx_settings) {
+            PIXI.loader
+                .add('fx_settings', 'assets/particles/fire/fire.json')
+                .add('fx_spritesheet', 'assets/particles/fire/revoltfx-spritesheet.json')
+                .load(() => {
+                    //console.log(loaders.baseUrl);
+                    this.initBundle();
+                });
+            return;
+        }
+        this.initBundle();
 
-       
+
+
+    }
+
+    async initBundle() {
+        this.candleFX.initBundle(PIXI.loader.resources.fx_settings.data);
+        this.emitter = this.candleFX.getParticleEmitter("fire_candle");
+        this.emitter.init(this, false, 1);
+        this.emitter.start();
+        this.emitter.infinite = true;
+        this.emitter.x = Game.app.screen.width / 2;
+        this.emitter.y = Game.app.screen.height / 2;
     }
 
     update() {
